90e5fed81607e2fbd938628a009c266e71632ae2
ast <anton.studen@gmail.com>
2026-04-21 00:32:39 +0300

    python app

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class AppHandler(BaseHTTPRequestHandler):
+    def do_GET(self): 
+        self.send_response(200)
+        #self.send_header('User-Agent', 'my-app/0.0.1')
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("Hello from Effective Mobile!".encode("utf-8"))
+
+server_hostname = ""
+server_port = 8080
+server_content = "Hello from Effective Mobile!"
+
+if __name__ == "__main__":
+    httpd = HTTPServer((server_hostname, server_port), AppHandler)
+    httpd.serve_forever()
